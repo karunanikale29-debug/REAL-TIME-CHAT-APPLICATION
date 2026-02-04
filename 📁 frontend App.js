962be427ae9ba1e363a9a80e5abb28fc0cf0ffe8
karunanikale29-debug <karunanikale29@gmail.com>
2026-02-04 08:39:57 -0500

    Create 📁 frontend App.js

---
+++ 📁 frontend App.js
@@ -0,0 +1,12 @@
+import Chat from "./Chat";
+
+function App() {
+  return (
+    <div className="app">
+      <h2>💬 Real-Time Chat App</h2>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
